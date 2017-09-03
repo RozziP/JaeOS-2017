020ee1297197ec36c092dd1677a9eee32cf9a67b
theRealMonning <rozzipeter@gmail.com>
2017-09-03 10:48:16 -0400

    Starter Files

---
+++ h/const.h
@@ -0,0 +1,15 @@
+#ifndef CONSTS
+#define CONSTS
+
+/**************************************************************************** 
+ *
+ * This header file contains utility constants & macro definitions.
+ * 
+ ****************************************************************************/
+#include "/usr/include/uarm/arch.h"
+#include "/usr/include/uarm/uARMconst.h"
+
+
+#define MAXPROC  		20
+
+#endif
---
+++ h/types.h
@@ -0,0 +1,30 @@
+#ifndef TYPES
+#define TYPES
+
+/**************************************************************************** 
+ *
+ * This header file contains utility types definitions.
+ * 
+ ****************************************************************************/
+
+#include "/usr/include/uarm/uARMtypes.h"
+
+/* process table entry type */
+typedef struct pcb_t {
+	/* process queue fields */
+    struct pcb_t   *p_next;		/* ptr to next entry     */
+
+    /* process tree fields */
+    struct pcb_t   *p_prnt, 		/* ptr to parent         */
+		   *p_child,		/* ptr to 1st child      */
+		   *p_sib,	        /* ptr to next sibling   */
+	
+    /* process status information */
+    state_t	   p_s;			/* processor state       */
+    int		   *p_semAdd;		/* ptr to semaphore on   */
+					/* which proc is blocked */
+}  pcb_t, *pcb_PTR;
+
+
+
+#endif
