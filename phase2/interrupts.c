279a18705f4a9594effaaccca3a07f39f7ba02ff
rozzip <rozzipeter@gmail.com>
2017-10-31 23:01:43 -0400

    interrupts skeleton and some meat

--- phase2/interrupts.c
+++ phase2/interrupts.c
@@ -4,7 +4,7 @@ interrupts.c
 */
 #include "../e/asl.e"
 #include "../e/pcb.e"
-#include "../e/globals.h"
+#include "../h/globals.h"
 #include "../h/const.h"
 #include "../h/types.h"
 #include "/usr/include/uarm/libuarm.h"
@@ -14,9 +14,55 @@ interruptHandler()
 {
     //int cause = (state_t*)INT_OLD->CP15_Cause;
     unsigned int cause = getCause();
-    
-}
+    int lineNum;
+    int deviceNum;
 
+    //Determine which line caused the interrupt
+    if(cause == /*line 3*/)
+    {
+       line = DISK;
+    }
+
+    else if(cause == /*line 4*/)
+    {
+       line = TAPE;
+    }
+
+    else if(cause == /*line 5*/)
+    {
+       line = NETWORK;
+    }
+
+    else if(cause == /*line 6*/)
+    {
+       line = PRINTER;
+    }
+
+    else if(cause == /*line 7*/)
+    {
+       line = TERMINAL;
+    }
+    else
+    {
+        PANIC();
+    }
+
+    //TODO find which device on the line caused the interrupt
+    deviceNum = //?
+    //TODO find the location of that device's registers
+
+    //copy status register and put in r0???
+    //set command field to ACK???
+
+    //signal the device's semaphore
+    int sem = sema4[/*device*/];
+    sem++;
+    pcb_PTR temp = removeBlocked(sem);
+    insertProcQ(&readyQueue, temp);
+    softBlockCnt--;
+
+    LDST(INT_OLD);
+}
 
 /*
 interruptHandler()
